8fa5c93e2d8d4f7cb69c63d00a5568cc93b642bb
ethan <wsvn53@gmail.com>
2021-07-07 16:22:43 +0800

    init ios project

--- Scrcpy/main.m
+++ Scrcpy/main.m
@@ -6,13 +6,8 @@
 //
 
 #import <UIKit/UIKit.h>
-#import "AppDelegate.h"
 
 int main(int argc, char * argv[]) {
-    NSString * appDelegateClassName;
-    @autoreleasepool {
-        // Setup code that might create autoreleased objects goes here.
-        appDelegateClassName = NSStringFromClass([AppDelegate class]);
-    }
-    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+    
+    return 0;
 }
